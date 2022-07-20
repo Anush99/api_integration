5d3f437459ae970992e082e362eccba17e90c3be
Anush Karapetyan <54039957+Anush99@users.noreply.github.com>
2022-07-20 15:29:04 +0400

    Update connection.py

--- connection.py
+++ connection.py
@@ -59,3 +59,64 @@ print('\n')
 
 
 
+def face_detect(image_url):
+
+  #  display the image using html url
+  image = io.imread(image_url)  
+  image = cv.cvtColor(image, cv.COLOR_BGR2RGB)
+
+  # get details about face detection of the given image and display it under the image
+  response = requests.get(
+      'https://api.imagga.com/v2/faces/detections?image_url=%s&return_face_attributes=1' % (image_url),
+      auth=(api_key, api_secret))
+  json_data = response.json()
+
+  try:
+    for i in range(len(['result'])):
+      # get coordinates of face
+        xmax = json_data['result']['faces'][i]['coordinates']['xmax']
+        xmin = json_data['result']['faces'][i]['coordinates']['xmin']
+        ymax = json_data['result']['faces'][i]['coordinates']['ymax']
+        ymin = json_data['result']['faces'][i]['coordinates']['ymin']
+
+        start_point = (xmin, ymin)
+        end_point = (xmax, ymax)
+        color = (0, 255, 0)
+        thickness = 1
+
+        # display image with green bounding box
+        image =  cv.rectangle(image, start_point, end_point, color, thickness)
+        cv2_imshow(image)
+        
+        if json_data['result']['faces'][i]['confidence'] > 50.0:
+            print("\033[1;32;47m Face Detected!")
+
+        # display detected person's age, gender and ethnicity
+        for i in range(len(['result'])):
+          age = (json_data['result']['faces'][i]['attributes'][i]['label'])
+          gender = (json_data['result']['faces'][i]['attributes'][i+1]['label'])
+          ethnicity = (json_data['result']['faces'][i]['attributes'][i+2]['label'])
+          print(f"\n Age: {age[0:-1]}\n Gender: {gender}\n Ethnicity: {ethnicity}")
+
+  
+  # if no face detected display image without bounding box
+  except IndexError:
+    cv2_imshow(image)
+    print("\033[1;31;47m No Face Detected.")
+  
+
+face_detect("https://i.ibb.co/7xcR7Dx/k-Z6axpb-Qs-M6bir-GOs-Tz-Evfbjs70-WM5x2flem-D1c-X.jpg")
+print('\n')
+
+face_detect("https://i.ibb.co/NZp2bXZ/xpn9-X3zc0qad-HSBy-Otg-N7-LFCKWAe-WBp-Xp9-Oxx-URA.jpg")
+print('\n')
+
+face_detect("https://i.ibb.co/dB1tK9C/ebe3f369e9df6c2dedfbb8ba58f5b591.jpg")
+print('\n')
+
+face_detect("https://i.ibb.co/YBDxxgZ/ee96150e6d4b0fdb7364ea258dd0e77b.jpg")
+print('\n')
+
+
+
+
